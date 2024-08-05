16c8b3339755e82314685193379762d046e42d22
Robert <161418088+Roberthsuan@users.noreply.github.com>
2024-08-05 09:50:59 +0800

    用個按鈕 透過spinner的getselectitemposition()的方法取得使用者選項index,將結果顯示在textview,實作onItemSelected

--- Spinner/app/src/main/java/com/example/spinner/MainActivity.java
+++ Spinner/app/src/main/java/com/example/spinner/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.spinner;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Spinner spinner=(Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+    }
+    public void buttonClick(View view){
+        Spinner spinner=(Spinner) findViewById(R.id.spinner);
+
+        String[] fruit=getResources().getStringArray(R.array.fruit);
+        int index = spinner.getSelectedItemPosition();
+
+        TextView result =(TextView) findViewById(R.id.tv_result);
+        result.setText("您選的是"+fruit[index]);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String[] fruit=getResources().getStringArray(R.array.fruit);
+
+        TextView result =(TextView) findViewById(R.id.tv_result);
+        result.setText("您選的是"+fruit[i]);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }

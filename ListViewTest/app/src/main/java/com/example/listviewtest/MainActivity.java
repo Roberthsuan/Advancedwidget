a1e2f6109cfe15cb0fcf4b2443b0e0fecf5bbf8f
Robert <161418088+Roberthsuan@users.noreply.github.com>
2024-08-07 10:34:11 +0800

    利用onitemclick方法，將使用者點及的項目用dialog,toast,textview呈現

--- ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -5,8 +5,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -32,8 +34,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         TextView tv=(TextView) findViewById(R.id.textView);
         String[] citiesArray= getResources().getStringArray(R.array.cities);
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("city");
+        dialog.setMessage("ur choice:"+citiesArray[i]);
+        dialog.setCancelable(true);
+        dialog.setPositiveButton("confirm",null);
+        dialog.setNeutralButton("cancel",null);
+        dialog.setNegativeButton("discard",null);
+        dialog.show();
 
-        tv.setText("您選擇的是:"+citiesArray[i]);
+        //tv.setText("您選擇的是:"+citiesArray[i]);
+        //Toast.makeText(this,"您選擇的是:"+citiesArray[i],Toast.LENGTH_LONG).show();
 
     }
 }
